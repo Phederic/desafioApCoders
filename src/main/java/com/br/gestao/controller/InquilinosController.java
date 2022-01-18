52d82f417b59efa9e135dc144c6bc715e3e5ba10
Phederic <87782988+Phederic@users.noreply.github.com>
2022-01-18 17:00:08 -0300

    Método de deletar inquilinos

--- src/main/java/com/br/gestao/controller/InquilinosController.java
+++ src/main/java/com/br/gestao/controller/InquilinosController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,4 +62,15 @@ public class InquilinosController {
 		return ResponseEntity.notFound().build();
 	}
 	
+	@DeleteMapping("/{id}")
+	@Transactional
+	@CacheEvict(value = "listaDeInquilinos", allEntries = true)
+	public ResponseEntity<InquilinosDto> deletar(@PathVariable Integer id){
+		Optional<Inquilinos> inquilinos = inquilinosRepository.findById(id);
+		if(inquilinos.isPresent()) {
+			inquilinosRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
 }
