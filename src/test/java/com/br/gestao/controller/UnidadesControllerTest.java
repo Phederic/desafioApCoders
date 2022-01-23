81b31e67181036a9def96625de0eeb623289c89b
Phederic <87782988+Phederic@users.noreply.github.com>
2022-01-23 20:46:59 -0300

    Teste de todas as rotas

---
+++ src/test/java/com/br/gestao/controller/UnidadesControllerTest.java
@@ -0,0 +1,123 @@
+package com.br.gestao.controller;
+
+import java.net.URI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class UnidadesControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testCadastrarComDados() throws Exception {
+		URI uri = new URI("/unidades");
+		String json = "{}";
+
+		mockMvc.perform(MockMvcRequestBuilders.post(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(201));
+	}
+
+	@Test
+	public void testCadastrarComDadosIncorretos() throws Exception {
+		URI uri = new URI("/unidades");
+		String json = "{dadasxa}";
+
+		mockMvc.perform(MockMvcRequestBuilders.post(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(400));
+	}
+
+	@Test
+	public void testListar() throws Exception {
+		URI uri = new URI("/unidades");
+		String json = "{}";
+
+		mockMvc.perform(MockMvcRequestBuilders.get(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(200));
+	}
+
+	@Test
+	public void testDeletar() throws Exception {
+		URI uri = new URI("/unidades/5");
+		String json = "{\"/{id}\"}";
+
+		mockMvc.perform(MockMvcRequestBuilders.delete(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(200));
+	}
+
+	@Test
+	public void testDeletarComIdQueNaoExiste() throws Exception {
+		URI uri = new URI("/unidades/70");
+		String json = "{\"/{id}\"}";
+
+		mockMvc.perform(MockMvcRequestBuilders.delete(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(404));
+	}
+
+	@Test
+	public void testDeletarSemId() throws Exception {
+		URI uri = new URI("/unidades");
+		String json = "{\"/{id}\"}";
+
+		mockMvc.perform(MockMvcRequestBuilders.delete(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(405));
+	}
+
+	@Test
+	public void testPesquisarPorID() throws Exception {
+		URI uri = new URI("/unidades/2");
+		String json = "{\"/{id}\"}";
+
+		mockMvc.perform(MockMvcRequestBuilders.get(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(200));
+	}
+
+	@Test
+	public void testModificar() throws Exception {
+		URI uri = new URI("/unidades/2");
+		String json = "{\"propietario\": \"Ramalho\"}";
+
+		mockMvc.perform(MockMvcRequestBuilders.put(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(200));
+	}
+
+	@Test
+	public void testModificarComIdQueNaoExiste() throws Exception {
+		URI uri = new URI("/unidades/88");
+		String json = "{\"propietario\": \"Ramalho\"}";
+
+		mockMvc.perform(MockMvcRequestBuilders.put(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(404));
+	}
+
+	@Test
+	public void testModificarSemId() throws Exception {
+		URI uri = new URI("/unidades/");
+		String json = "{\"propietario\": \"Ramalho\"}";
+
+		mockMvc.perform(MockMvcRequestBuilders.put(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(405));
+	}
+
+	@Test
+	public void testModificarComDadosIncorretos() throws Exception {
+		URI uri = new URI("/unidades/4");
+		String json = "{hahahaxa}";
+
+		mockMvc.perform(MockMvcRequestBuilders.put(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().is(400));
+	}
+
+}
